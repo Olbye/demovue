c4caa746a8a0413112603a3bbb3fc2f52162ba9a
Olybe <1812868389@qq.com>
2020-09-19 18:10:56 +0800

    test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -10,6 +10,7 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  // eslint-disable-next-line prettier/prettier
   name: 'Home',
   components: {
     HelloWorld
